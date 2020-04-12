7831c5ac9d71eee393057f8d702e5bab3117280d
pinyoo Thotaboot <pinyoo_too@hotmail.com>
2020-04-12 20:04:00 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,11 @@
 
-YEAR = 2020
+import datetime
+x = datetime.datetime.now()
+
+YEAR = x.year
 # Database Excel file
 LOC = 'database.xls' # Do not Edit
 LOGIN_URL = "http://nsmart.nhealth-asia.com/MTDPDB01/index.php" # Do not Edit
 URL = "http://nsmart.nhealth-asia.com/MTDPDB01/reports/master_kpi.php?sp_tyear={}&sp_branchid=10024&sp_dept=1002401".format(YEAR) # Do not Edit
 URL_ONLINE_REPAIR = "http://nsmart.nhealth-asia.com/MTDPDB01/jobs/BJOBA_01online.php" # Do not Edit
 LINE_URL = "https://notify-api.line.me/api/notify" # Do not Edit
-
-
-CSI_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfWwxjNE3ZSnbOm7jHZlKz65RhIDjUKHLR9OrGLgWe8EPSfYQ/viewform?usp=sf_link"
-LINE_TOKEN = {
-    "department name if not exter site name ex. SSH": "Line Token",
-}
-USERNAME = "Site 4 username ex. SSH4....." 
-PASSWORD = "ex. 55555" 
-SLEEP = 30 # second
-SITE_NAME = "Site name ex. SSH" 
